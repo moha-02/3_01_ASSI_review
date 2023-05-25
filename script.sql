dab679fde6bbfba89dc75201bac1f02a6915a7d9
moha-02 <119495982+moha-02@users.noreply.github.com>
2023-05-25 19:16:47 +0200

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,36 @@
+--- THREE WAY LOGIC
+select null;
+
+select null = 1;
+
+select null = null;
+
+select (null=1) or (1=1);
+
+select (null=1) and (1=0);
+
+-------------------------------------------------
+
+--- AGGREGATION AND GROUPING
+SELECT 
+    product,
+    AVG(unit_price) AS avg_price,
+    SUM(quantity) AS tot_pieces,
+    SUM(total) AS total_gain
+FROM sales
+GROUP BY product
+HAVING SUM(total) > 40000
+ORDER BY total_gain DESC
+
+-------------------------------------------------
+
+--- SUBQUERYS
+select name
+from Person
+where gender='female'
+and exists (select Eats.name from Eats
+            where Eats.name=Person.name
+            and pizza='pepperoni')
+and exists (select Eats.name from Eats
+            where Eats.name=Person.name
+            and pizza='mushroom')
